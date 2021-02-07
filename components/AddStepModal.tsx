9c720bb92d97bbe35844a8b94648cfad30d52132
okelvynsantana <kelvyn.santana@bbnk.com.br>
2021-02-07 16:19:53 -0300

    refact: Alter search services engine

--- components/AddStepModal.tsx
+++ components/AddStepModal.tsx
@@ -1,4 +1,4 @@
-import { AddIcon } from '@chakra-ui/icons'
+// import { AddIcon } from '@chakra-ui/icons'
 import {
   Button,
   Flex,
@@ -9,6 +9,7 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -34,16 +35,97 @@ const AddStepModal: React.FC<RenderServicesModalProps> = ({
   const [searchService, setSearchService] = useState('')
   const [resultServices, setResultServices] = useState([])
   const [stepName, setStepName] = useState('')
+  const [loading, setLoading] = useState(false)
 
-  const handleSearchServices = useCallback(async () => {
-    setResultServices([])
+  // const handleSearchServices = useCallback(async () => {
+  //   setResultServices([])
+  //   const result = await search(
+  //     `/api/SearchProducts?searchTerm=${searchService}`
+  //   )
+  //   setResultServices(result.data)
+  //   console.log(resultServices)
+  //   setSearchService('')
+  // }, [searchService])
+
+  const RenderTable = () => {
+    return (
+      <>
+        {!resultServices.length ? (
+          <RenderEmptyContent />
+        ) : (
+          <Table variant="simple" flexDir="row" marginTop="25px">
+            <Thead>
+              <Tr>
+                <Th>Código</Th>
+                <Th>Serviço</Th>
+                <Th>UND</Th>
+                <Th>COEF</Th>
+                <Th>QTD</Th>
+                <Th>Preço Unitário</Th>
+                <Th>Preço total</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {resultServices.map(result => (
+                <Tr key={result.compositionCode}>
+                  <Td>{result.compositionCode}</Td>
+                  <Td maxW="100px" textOverflow="ellipsis" overflow="hidden">
+                    {result.compositionDescription}
+                  </Td>
+                  <Td>UND</Td>
+                  <Td>COEF</Td>
+                  <Td>
+                    <Input maxW="50px" type="number"></Input>
+                  </Td>
+                  <Td>1</Td>
+                  <Td>2</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        )}
+      </>
+    )
+  }
+
+  const RenderLoading = () => {
+    return (
+      <Flex
+        flexDir="column"
+        alignItems="center"
+        justifyContent="center"
+        mt="40px"
+      >
+        <Spinner />
+        <Text>Carregando...</Text>
+      </Flex>
+    )
+  }
+  const RenderEmptyContent = () => {
+    return (
+      <Flex
+        flexDir="column"
+        alignItems="center"
+        justifyContent="center"
+        mt="40px"
+      >
+        <Text>
+          Não há resultados para o termo pesquisado, tente com outro termo
+        </Text>
+      </Flex>
+    )
+  }
+
+  const onChangeHandler = useCallback(async (e: any) => {
+    setLoading(true)
+    setSearchService(e.target.value)
     const result = await axios.get(
-      `/api/SearchProducts?searchTerm=${searchService}`
+      `/api/SearchProducts?searchTerm=${e.target.value}`
     )
     setResultServices(result.data)
-    console.log(resultServices)
-    setSearchService('')
-  }, [searchService])
+    setLoading(false)
+  }, [])
+  const handleAddNewStep = useCallback(() => {}, [])
   return (
     <Modal
       isOpen={isOpen}
@@ -54,11 +136,13 @@ const AddStepModal: React.FC<RenderServicesModalProps> = ({
     >
       <ModalOverlay />
       <ModalContent padding="30px">
-        <ModalHeader border="red 1px solid" mt="10px">
-          <Text>
-            {stepName ? `Etapa: ${stepName}` : 'Adicionar nova etapa'}
-          </Text>
-          <Button>Salvar</Button>
+        <ModalHeader>
+          <Flex justifyContent="space-between">
+            <Text>
+              {stepName ? `Etapa: ${stepName}` : 'Adicionar nova etapa'}
+            </Text>
+            <Button onClick={handleAddNewStep}>Salvar Etapa</Button>
+          </Flex>
         </ModalHeader>
         <ModalCloseButton borderRadius="full" />
         <ModalBody>
@@ -74,55 +158,26 @@ const AddStepModal: React.FC<RenderServicesModalProps> = ({
           <Text mt="25px" fontWeight="600">
             Pesquisar Serviços
           </Text>
-          <Flex mt="25px">
+          <Flex mt="25px" width="70%" flexDir="column">
             <Input
               placeholder="Digite o nome do serviço"
               value={searchService}
-              onChange={e => setSearchService(e.target.value)}
+              onChange={e => onChangeHandler(e)}
             />
-            <Button
+
+            {/* <Button
               background="brand.primary"
               color="#FFF"
               ml="10px"
               onClick={handleSearchServices}
             >
               Buscar
-            </Button>
+            </Button> */}
           </Flex>
-          <Table variant="simple" flexDir="row" marginTop="25px">
-            <Thead>
-              <Tr>
-                <Th>Código</Th>
-                <Th>Serviço</Th>
-                <Th>UND</Th>
-                <Th>COEF</Th>
-                <Th>QTD</Th>
-                <Th>Ações</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {resultServices.map(result => (
-                <Tr key={result.compositionCode}>
-                  <Td>{result.compositionCode}</Td>
-                  <Td maxW="100px" textOverflow="ellipsis" overflow="hidden">
-                    {result.compositionDescription}
-                  </Td>
-                  <Td>UND</Td>
-                  <Td>COEF</Td>
-                  <Td>
-                    <Input maxW="50px" type="number"></Input>
-                  </Td>
-                  <Td>
-                    <AddIcon></AddIcon>
-                  </Td>
-                </Tr>
-              ))}
-            </Tbody>
-          </Table>
+          {loading ? <RenderLoading /> : <RenderTable />}
         </ModalBody>
       </ModalContent>
     </Modal>
   )
 }
-
 export default AddStepModal
